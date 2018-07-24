3d8d0a749bd6c92bc1cb88e5a85ba476f3d002be
Jonathan Claudius <jclaudius@mozilla.com>
2018-07-24 16:32:02 -0400

    Add service-based decisions

--- run.py
+++ run.py
@@ -16,8 +16,6 @@ if "http" in fqdn:
 
 #TODO: make this dependant on NMAP results/protocol fingerprints
 # In the mean time, just toggle and rebuild it, maybe we'll just take a URL as a seed? dunno.
-scheme = "https"
-
 output_path = "/app/results/" + fqdn + "/"
 
 # Create a location to store our outputs
@@ -27,12 +25,25 @@ except:
   os.mkdir(output_path) 
 
 # Do procedure NMAP scans (save output to /app/results)
-os.system("nmap -v -Pn -sT -n -p 443 --open -T4 -oX " + output_path + "scan_tcp_" + fqdn + ".xml " + fqdn)
-os.system("nmap -v -Pn -sU -sV -n -p 17,19,53,67,68,123,137,138,139,161,162,500,520,646,1900,3784,3785,5353,27015,27016,27017,27018,27019,27020,27960 --open -T4 -oX " + output_path + "scan_udp_" + fqdn + ".xml " + fqdn)
+os.system("nmap -v -Pn -sT -n --top-ports 1000 --open -T4 -oA " + output_path + "scan_tcp_" + fqdn + " " + fqdn)
+# os.system("nmap -v -Pn -sU -sV -n -p 17,19,53,67,68,123,137,138,139,161,162,500,520,646,1900,3784,3785,5353,27015,27016,27017,27018,27019,27020,27960 --open -T4 -oA " + output_path + "scan_udp_" + fqdn + " " + fqdn)
 
-# Do procedure dirb scans (save output to directory outside container)
-command = "/app/vendor/dirb222/dirb " + scheme + "://" + fqdn + "/ /app/vendor/dirb222/wordlists/common.txt -o /app/results/" + fqdn + "/" + scheme + "_dirb_common.txt"
-os.system(command)
+# Do procedure dirb/ssh scans (save output to directory outside container)
+nmap_tcp_gnmap_file = open("/app/results/" + fqdn + "/scan_tcp_" + fqdn + ".gnmap", "r")
+lines = nmap_tcp_gnmap_file.readlines()
+for line in lines:
+  if (("Host:" in line) and ("Ports:" in line) and ("443/open/tcp" in line)):
+    print("https is open, so we'll dirb it...")
+    command = "/app/vendor/dirb222/dirb https://" + fqdn + "/ /app/vendor/dirb222/wordlists/common.txt -o /app/results/" + fqdn + "/https_dirb_common.txt"
+    os.system(command)
+  if (("Host:" in line) and ("Ports:" in line) and ("80/open/tcp" in line)):
+    print("http is open, so we'll dirb it...")
+    command = "/app/vendor/dirb222/dirb http://" + fqdn + "/ /app/vendor/dirb222/wordlists/common.txt -o /app/results/" + fqdn + "/http_dirb_common.txt"
+    os.system(command)
+  if (("Host:" in line) and ("Ports:" in line) and ("22/open/tcp" in line)):
+    print("ssh is open, so we'll ssh_scan it...")
+    command = "ssh_scan -t " + fqdn + " -o /app/results/" + fqdn + "/ssh_scan.txt"
+    os.system(command)
 
 # Do procedure X (save output to /app/results)
 
